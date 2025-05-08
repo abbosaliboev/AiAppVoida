0cc636a4cdbba484b9b30b2aa0718f8a43b8b966
Kim Taeyeong <xodud7737@gmail.com>
2025-05-08 10:33:17 +0900

    _

--- app/src/main/java/com/example/team_voida/Home/HomeSever.kt
+++ app/src/main/java/com/example/team_voida/Home/HomeSever.kt
@@ -9,7 +9,7 @@ suspend fun HomePopularCall():List<Popular>?{
     try{
         val url = URL(" https://fluent-marmoset-immensely.ngrok-free.app") // edit1
         val connection = url.openConnection() as java.net.HttpURLConnection
-        connection.requestMethod = "POST" // edit2
+        connection.requestMethod = "GET" // edit2
 
         if(connection.responseCode == HttpURLConnection.HTTP_OK){
             val inputStream = connection.inputStream.bufferedReader().use{it.readText()}
